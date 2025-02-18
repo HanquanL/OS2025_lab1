f6d1367fbd893249ddbb5991e60416aa4e246cf0
HanquanL <HanquanL0115@gmail.com>
2025-02-17 22:54:47 -0500

    reorg tokenizer

--- linker.cpp
+++ linker.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-
+int getToken(string fileName);
 
 int main(int argc, char* argv[])
 {
-    string fileName, token;
-    int lineNumber = 0, currentOffset = 0;
+    string fileName;
+    
     
     if (argc != 2){
         cout << "Invalid file name!" << endl;
@@ -18,27 +18,34 @@ int main(int argc, char* argv[])
    }else{
         fileName = argv[1];
    }
+    getToken(fileName);
+   
 
-   ifstream inputFile(fileName);
-   if(!inputFile){
-        cout << "Failed to open file: " << fileName << endl;
-        return 1;
-   }
-   string line;
-   while(getline(inputFile , line)){
-        lineNumber++;
-        istringstream lineToken(line);
-        size_t tokenStart = 0;
-        while(lineToken >> token){
-            size_t found = line.find(token, tokenStart);
-            currentOffset = found;
-            if(found != string::npos){
-                printf("token:<%s> position=%d:%zd\n", token.c_str(), lineNumber, found);
-                tokenStart = found + token.length();
-            }
-        }  
-   }
-   printf("EOF position = %d:%d\n", lineNumber, currentOffset); 
+    return 0;
+}
 
+int getToken(string fileName){
+    int lineNumber = 0, currentOffset = 0;
+    string token,line;
+    ifstream inputFile(fileName);
+    if(!inputFile){
+            cout << "Failed to open file: " << fileName << endl;
+            return 1;
+    }
+    
+    while(getline(inputFile , line)){
+            lineNumber++;
+            istringstream lineToken(line);
+            size_t tokenStart = 0;
+            while(lineToken >> token){
+                size_t found = line.find(token, tokenStart);
+                currentOffset = found;
+                if(found != string::npos){
+                    printf("token:<%s> position=%d:%zd\n", token.c_str(), lineNumber, found);
+                    tokenStart = found + token.length();
+                }
+            }  
+    }
+    printf("EOF position = %d:%d\n", lineNumber, currentOffset); 
     return 0;
 }
