23d4027d24b8e0dae3880949fff215d35818b7ce
HanquanL <HanquanL0115@gmail.com>
2025-02-17 20:01:28 -0500

    working on tokenizer

--- linker.cpp
+++ linker.cpp
@@ -8,6 +8,12 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-   
+    string fileName;
+    if (argc != 2){
+        cout << "Invalid file name" << endl;
+        exit(0);
+   }else{
+        fileName = argv[1];
+   }
     return 0;
 }
