26c9b99f3d73d7e9b0ed86f2c8f05c24fcf9ef02
HanquanL <HanquanL0115@gmail.com>
2025-02-17 20:32:25 -0500

    completed open file, and read from file

--- linker.cpp
+++ linker.cpp
@@ -8,12 +8,25 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    string fileName;
+    string fileName, token;
+    int lineNumber, lineOffset;
+    
     if (argc != 2){
-        cout << "Invalid file name" << endl;
+        cout << "Invalid file name!" << endl;
         exit(0);
    }else{
         fileName = argv[1];
    }
+
+   ifstream inputFile(fileName);
+   if(!inputFile){
+        cout << "Failed to open file: " << fileName << endl;
+        return 1;
+   }
+   string word;
+   while(inputFile >> word){
+        cout << word << endl;
+   }
+
     return 0;
 }
